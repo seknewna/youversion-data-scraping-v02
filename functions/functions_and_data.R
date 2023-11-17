59f30f5e8e95b24719c10276ccb87137ad03988a
seknewna <seknewna@gmail.com>
2023-11-17 13:25:13 +0200

    Scraping all books

--- functions/functions_and_data.R
+++ functions/functions_and_data.R
@@ -4,16 +4,16 @@ library(tidyverse)
 library(openxlsx)
 library(stringr)
 
-# all_books <- c("GEN", "EXO", "LEV", "NUM", "DEU", "JOS", "JDG", "RUT", "1SA", 
-#                "2SA", "1KI", "2KI", "1CH", "2CH", "EZR", "NEH", "EST", "JOB",
-#                "PSA", "PRO", "ECC", "SNG", "ISA", "JER", "LAM", "EZK", "DAN", 
-#                "HOS", "JOL", "AMO", "OBA", "JON", "MIC", "NAM", "HAB", "ZEP", 
-#                "HAG", "ZEC", "MAL", "MAT", "MRK", "LUK", "JHN", "ACT", "ROM", 
-#                "1CO", "2CO", "GAL", "EPH", "PHP", "COL", "1TH", "2TH", "1TI", 
-#                "2TI", "TIT", "PHM", "HEB", "JAS", "1PE", "2PE", "1JN", "2JN", 
-#                "3JN", "JUD", "REV")
+all_books <- c("GEN", "EXO", "LEV", "NUM", "DEU", "JOS", "JDG", "RUT", "1SA",
+               "2SA", "1KI", "2KI", "1CH", "2CH", "EZR", "NEH", "EST", "JOB",
+               "PSA", "PRO", "ECC", "SNG", "ISA", "JER", "LAM", "EZK", "DAN",
+               "HOS", "JOL", "AMO", "OBA", "JON", "MIC", "NAM", "HAB", "ZEP",
+               "HAG", "ZEC", "MAL", "MAT", "MRK", "LUK", "JHN", "ACT", "ROM",
+               "1CO", "2CO", "GAL", "EPH", "PHP", "COL", "1TH", "2TH", "1TI",
+               "2TI", "TIT", "PHM", "HEB", "JAS", "1PE", "2PE", "1JN", "2JN",
+               "3JN", "JUD", "REV")
 
-all_books = c("GEN")
+# all_books = c("GEN")
 
 # function that will detect the next verse if it exists
 next_verse <- function(prev_verse){
